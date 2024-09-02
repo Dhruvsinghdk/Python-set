1c099ce645aebe22234ebe2d20861bbf925d35c7
Dhruvsinghdk <re621006@gmail.com>
2024-09-02 22:03:14 +0530

    new

---
+++ Setmethord.py
@@ -0,0 +1,21 @@
+
+n = {1,"apna","java","php",123}
+n2 = {1,2,"apna","podi",6,7}
+n.add(5)
+print(n)
+
+remove();
+
+n.remove(2)
+print(n)
+
+clear();
+n.clear()
+print(n)
+
+print(n.pop())
+print(n.pop())
+
+print(n.union(n2))
+
+print(n.intersection(n2))
---
+++ set.py
@@ -0,0 +1,6 @@
+set = {1,2,3,4,44,4,4}
+print(set)
+ for empty set
+
+abc = set()
+print(type(abc))
